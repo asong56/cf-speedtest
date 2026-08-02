bf6033dceaf89d6bad78f951a26d64f55fc14595
Ansel <238927804+asong56@users.noreply.github.com>
2026-08-02 14:50:29 -0400

    utils

---
+++ utils/color.go
@@ -0,0 +1,13 @@
+package utils
+
+import "github.com/fatih/color"
+
+var (
+	Red     = color.New(color.FgRed)
+	Green   = color.New(color.FgGreen)
+	Yellow  = color.New(color.FgYellow)
+	Blue    = color.New(color.FgBlue, color.Bold)
+	Magenta = color.New(color.FgMagenta)
+	Cyan    = color.New(color.FgHiCyan, color.Bold)
+	White   = color.New(color.FgWhite)
+)
